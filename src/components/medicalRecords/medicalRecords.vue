258c157995fada8ef0f8c6ede3670ca697e04ae6
k0xxx <androidk0xxx@gmail.com>
2017-08-11 17:23:50 +0300

    server compress

--- src/components/medicalRecords/medicalRecords.vue
+++ src/components/medicalRecords/medicalRecords.vue
@@ -1,8 +1,12 @@
 <template>
 	<div id="view" class="noSideBar">
-		<div id="medicalRecords" class="card">
-			{{ msg }}
-		</div>
+			<div id="medicalRecords" v-bind:class="{fullWidget: isFullWidget}">
+        <widgetWeight v-if="activeWidgets.wWeight.show" :showWidget="activeWidgets.wWeight.show" :isFullWidget="activeWidgets.wWeight.full" @toogle="toogle('wWeight')"></widgetWeight>
+        <widgetGlucose v-if="activeWidgets.wGlucose.show" :showWidget="activeWidgets.wGlucose.show" :isFullWidget="activeWidgets.wGlucose.full" @toogle="toogle('wGlucose')"></widgetGlucose>
+        <widgetBloodpressure v-if="activeWidgets.wBloodpressure.show" :showWidget="activeWidgets.wBloodpressure.show" :isFullWidget="activeWidgets.wBloodpressure.full" @toogle="toogle('wBloodpressure')"></widgetBloodpressure>
+        <widgetCholesterol v-if="activeWidgets.wCholesterol.show" :showWidget="activeWidgets.wCholesterol.show" :isFullWidget="activeWidgets.wCholesterol.full" @toogle="toogle('wCholesterol')"></widgetCholesterol>
+        <widgetHeight v-if="activeWidgets.wHeight.show" :showWidget="activeWidgets.wHeight.show" :isFullWidget="activeWidgets.wHeight.full" @toogle="toogle('wHeight')"></widgetHeight>
+      </div>
 	</div>
 </template>
 
