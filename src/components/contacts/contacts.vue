d4bb3a990946b98ad8d9e7b5aa8ad10eca31592d
k0xxx <androidk0xxx@gmail.com>
2017-08-23 17:42:47 +0300

    contacts update

--- src/components/contacts/contacts.vue
+++ src/components/contacts/contacts.vue
@@ -1,7 +1,11 @@
 <template>
 	<div id="view" class="withSideBar">
+		<div>
+			<form action="">
+				<input type="text" name="search" placeholder="Поиск..." v-model="contactsSearch">
+			</form>
+		</div>
 		<div id="contacts">
-			<p>{{test}}</p>
 			<contactItem v-for="contact in contactsList" v-bind:key="contact._id" v-bind:contact="contact"></contactItem>
 			<infinite-loading :on-infinite="onInfinite" ref="infiniteLoading">
 				<span slot="no-more">Всё загружено!</span>
@@ -22,13 +26,14 @@ export default {
 			page: 1,
 			endpoint: 'http://api.clinilink.org/api/contacts',
 			contactsList: [],
-			test: '1'
+			contactsType: '',
+			contactsSearch: ''
 		}
 	},
 	methods: {
 		onInfinite() {
 			var options = {
-				params: {page: this.page}
+				params: {page: this.page, contactsType: this.contactsType, contactsSearch: this.contactsSearch}
 			}
 			this.$http.get(this.endpoint, options).then((response) => {
 				if (response.data.contactsList.length) {
@@ -40,18 +45,17 @@ export default {
 				}
 		  });
 		},
-	},
-	created: function(){
-		
-	},
-	route: {
-		data(){
-			this.test = this.$route.query;
+		getQuery(){
+			this.contactsType = this.$route.query.type;
+			this.contactsList = [];
+			this.page = 1;
+      		this.$nextTick(() => {
+        		this.$refs.infiniteLoading.$emit('$InfiniteLoading:reset');
+      		});
 		}
-    	/*data () {
-      		// handle updated route
-      		this.queryItems(this.$route.query);
-    	}*/
-  	}
+	},
+	watch: {
+    	'$route': 'getQuery'
+  	},
 }      
 </script>  
--- src/components/contacts/filterContact.vue
+++ src/components/contacts/filterContact.vue
@@ -1,11 +1,23 @@
 <template>
 	<aside class="sideBar">
-        	<div id="Contact_filter">
-			<a href="#" class="list-item-filter p-50 active_filter">Все контакты</a> 
-			<router-link :to="{ name: 'contacts', query: { userId: 123 }}">Тестов</router-link>
-			<a href="#" class="list-item-filter p-50">Пациенты</a>
-			<a href="#" class="list-item-filter p-50">Специалисты</a>
-			<a href="#" class="list-item-filter p-50">Врачи</a> </div>
-        	</div>
+        <div id="Contact_filter">
+			<router-link :to="{ name: 'contacts'}" class="list-item-filter p-50 active_filter">Мои контакты</router-link>
+			<router-link :to="{ name: 'contacts', query: { type: 'in_requests' }}" class="list-item-filter p-50">Входящие заявки</router-link>
+			<router-link :to="{ name: 'contacts', query: { type: 'out_requests' }}" class="list-item-filter p-50">Исходящие заявки</router-link>
+			<router-link :to="{ name: 'contacts', query: { type: 'subsribers' }}" class="list-item-filter p-50">Подписки</router-link>
+			<div class="mt-50">
+				<label for="">Тип контакта</label>
+				<select class="w-100 py-50">
+					<option value="">Все</option>
+					<option value="">Пациенты</option>
+					<option value="">Специалисты</option>
+					<option value="">Врачи</option>
+				</select>
+			</div>
+			
+			<!--<router-link :to="{ name: 'contacts', query: { userId: 123313 }}" class="list-item-filter p-50">Пациенты</router-link>
+			<router-link :to="{ name: 'contacts', query: { userId: 1213 }}" class="list-item-filter p-50">Специалисты</router-link>
+			<router-link :to="{ name: 'contacts', query: { userId: 1253 }}" class="list-item-filter p-50">Врачи</router-link>-->
+        </div>
 	</aside>
 </template>
