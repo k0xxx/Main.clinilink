5d847a4ed230f0c72875a28f707d54cca54f206c
k0xxx <androidk0xxx@gmail.com>
2017-08-25 01:18:55 +0300

    url medical_records update

--- src/router/index.js
+++ src/router/index.js
@@ -70,7 +70,7 @@ var router =  new VueRouter({
 		}, 
 		{path: '/library/:classUrl/:categoryUrl/:itemUrl', name: 'libraryItemPage', components: {default: LibraryItem, rightBar: LibraryItem, sideBar: LibrarySidebar}},
 		{path: '/measurements', name: 'measurements', component: Measurements},
-		{path: '/medicalRecords', name: 'medicalRecords', component: MedicalRecords},
+		{path: '/medical_records', name: 'medicalRecords', component: MedicalRecords},
 		{path: '/questions', name: 'questions', components: {default: Questions, sideBar: QuestionsSideBar}},
 		{path: '/messages', name: 'messages', component: Messages},
 		{path: '/consultations', name: 'consultations', component: Consultations},
